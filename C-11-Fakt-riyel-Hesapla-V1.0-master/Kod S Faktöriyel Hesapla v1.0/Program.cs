8d9ef0c7b552a70a06fc5c8a5ec5ce0c8fc63b37
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-23 02:24:20 +0300

    Initial commit

---
+++ C-11-Fakt-riyel-Hesapla-V1.0-master/Kod S Faktöriyel Hesapla v1.0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kod_S_Faktöriyel_Hesapla_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            double Sonuc = 1;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Sonuc *= i;
+            }
+
+            Console.WriteLine();
+
+            Console.Write(n + "!=" + Sonuc);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharp_Console_10_Faktoriyel_Hesapla_v.1.0
+ 
